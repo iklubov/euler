0ce964862b404ceee34c68a03c34f5f176469ee3
Ilya <iklubov@gmail.com>
2017-07-24 21:02:47 +0300

    second

--- euler.py
+++ euler.py
@@ -2,6 +2,8 @@
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
 import math
+from functools import reduce
+
 
 from utils import reverseNumber
 
@@ -106,6 +108,49 @@ def task7():
         print(len(primeNums))
     print (primeNums[0], primeNums[-1])
 
+def task8():
+    numStr = '7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450'
+    maxProd = 1
+    adjNum = 13
+    offset = 0
+    maxProdString = ''
+    while (len(numStr) - offset) > adjNum:
+        thisProd = numStr[offset:offset+adjNum]
+        thisProdValue = reduce(lambda x, y: int(x)*int(y), thisProd)
+        if thisProdValue > maxProd:
+            maxProd = thisProdValue
+            maxProdString = thisProd
+        offset = offset + 1
+    print(maxProd, maxProdString)
+
+def task9():
+    for k in range(1, 999):
+        for j in range(1, k):
+            for i in range(1, j):
+                if i**2 + j**2 == k**2:
+                    print(i,j,k, sum([i,j,k]))
+                    if sum([i,j,k]) == 1000:
+                        print(i*j*k)
+                        return
+
+    # for i in range(1,999999999):
+    #     abc = [0]*3
+    #     num = i
+    #     counter = 0
+    #     while num > 0:
+    #         #print(num)
+    #         abc[counter] = int(num % 1000)
+    #         num = math.floor(num / 1000)
+    #         counter += 1
+    #     #print(i, abc)
+    #     if sum(abc) != 1000:
+    #         continue
+    #     print(i)
+    #     squares = [x*x for x in abc]
+    #     if (squares[0] + squares[1]) == squares[2]:
+    #         print('decision', abc)
 
 
-task7()
+
+
+task9()
